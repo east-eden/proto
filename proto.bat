a5fa14e0c05660df4ece60c71747cf5b9c83e276
chenfei.zhang <chenfei.zhang@centurygame.com>
2021-04-28 15:27:36 +0800

    客户端批处理工具

--- proto.bat
+++ proto.bat
@@ -1,18 +1,8 @@
 @echo off
-::set outDir=%~1
-::set input=%~2
-for %%i in (global/*.proto) do (
-   echo gen %%i
-   protoc.exe --proto_path="global" --csharp_wxb_out=output  input
-)
-go skip
-for /f "delims=" %%d in ('dir /ad/b/x "global"') do (
-   echo %%d
-   for %%i in (global/%%d/*.proto) do (
-	  echo gen %%d/%%i
-	  protoc.exe --csharp_wxb_out=output  global/%%d/%%i
-	)
-)
-:skip
-echo finish... 
-pause
+set outDir=%~1
+set input=%~2
+::echo gen %input%
+echo.|protoc.exe --proto_path="global" --csharp_wxb_out=%outDir%  global/%input%
+::echo finish... 
+
+::pause
