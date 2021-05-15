108a5dd831c777be11f7cad07d0087eec9da0f84
chenfei.zhang <chenfei.zhang@centurygame.com>
2021-05-15 18:59:41 +0800

    proto.bat

--- proto.bat
+++ proto.bat
@@ -3,7 +3,7 @@ set outDir=%~1
 ::set outDir=output
 for %%i in (global/*.proto) do (
     echo gen %%i
-    echo.|protoc.exe --proto_path="global" --csharp_out=%outDir%  global/%%i
+    echo.|protoc.exe --proto_path="global" --csharp_wxb_out=%outDir%  global/%%i
 )
 ::echo finish... 
 ::pause
